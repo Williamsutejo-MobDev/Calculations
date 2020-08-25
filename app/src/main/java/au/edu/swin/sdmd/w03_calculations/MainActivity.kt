301791c3509fb88086570eb0fbf30c3e3fdea4ea
williamsutejo-mobdev <102346858@student.swin.edu.au>
2020-08-25 21:34:08 +1000

    first commit

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,14 +17,17 @@ class MainActivity : AppCompatActivity() {
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
 
+
+
         val equals = findViewById<Button>(R.id.equals)
         equals.setOnClickListener { _ ->
             val result = add(number1.text.toString(), number2.text.toString())
+            equals.setText(result.toString())
             // TODO: show result on the screen
         }
     }
 
     // adds two numbers together
-    private fun add(number1: String, number2: String) = number1 + number2
+    private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
 
 }
