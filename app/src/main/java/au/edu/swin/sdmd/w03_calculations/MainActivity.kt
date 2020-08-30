883e74bdbf290b72716401431c4f16a69e00fff8
williamsutejo-mobdev <102346858@student.swin.edu.au>
2020-08-30 17:42:32 +1000

    2nd commit

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -2,32 +2,59 @@ package au.edu.swin.sdmd.w03_calculations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var operator = "plus"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
+        val answer =  findViewById<TextView>(R.id.answer)
 
 
 
         val equals = findViewById<Button>(R.id.equals)
         equals.setOnClickListener { _ ->
-            val result = add(number1.text.toString(), number2.text.toString())
-            equals.setText(result.toString())
+            val result = when (operator) {
+                "plus" -> add(number1.text.toString(), number2.text.toString())
+                "mult" -> mult(number1.text.toString(), number2.text.toString())
+                else -> add(number1.text.toString(), number2.text.toString())
+
+            }
             // TODO: show result on the screen
+            answer.setText(result.toString())
+        }
+
+
+    }
+    fun onRadioButtonClicked(view: View){
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+            when (view.getId()) {
+                R.id.RadioPlus ->
+                    if (checked){
+                        operator = "plus"
+                    }
+
+                R.id.RadioMult ->
+                    if (checked){
+                        operator = "mult"
+                    }
+            }
         }
     }
-
     // adds two numbers together
     private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
+    private fun mult(number1: String, number2: String) = number1.toInt() * number2.toInt()
 
 }
